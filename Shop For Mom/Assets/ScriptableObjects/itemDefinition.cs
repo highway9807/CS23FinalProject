682168a74c483bfe5a73c02bd39f3fe8fd9e54c1
highway9807 <milogoldstein@gmail.com>
2026-03-08 17:11:17 -0400

    Item Definition scriptableObject
    Well use this scriptable object to create grocery store items without touching the code.

---
+++ Shop For Mom/Assets/ScriptableObjects/itemDefinition.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/* the thing in the [] is called an attribute, they have many functions. This
+ * one creates a menu shortcut in the unity editor allowing us to create a
+ * new instance of an item (then fill in its id, name, and sprite) without
+ * ever touching code! To do so:
+ *          
+ *      right click inside any folder in the Project tab -> create -> Scriptable
+ *      Objects -> itemDefinition.
+*/
+[CreateAssetMenu(fileName = "itemDefinition", 
+                 menuName = "Scriptable Objects/itemDefinition")]
+public class itemDefinition : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public Sprite sprite;
+}
