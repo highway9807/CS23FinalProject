c933dd1d062cc9631645d3d1301e70e4d43fd15c
highway9807 <milogoldstein@gmail.com>
2026-03-09 13:00:41 -0400

    GameHandler created (work_milo)
    I also created a preliminary script. Since nothing is set yet, treat these as initial infrastructure. I have not yet connected the GameHandler.cs Script I've made to the GameObject, nor have I made these into a prefab.

---
+++ Shop For Mom/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+    // TODO: add player gameObject (pirvate, serialize)
+    // TODO: add any TMP objects (private, serialize)
+    // TOdO: initialize any gamestate variables we may need (private, serialize)
+
+
+    // add scene names to easily switch using scene manager (private, serialize)
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
