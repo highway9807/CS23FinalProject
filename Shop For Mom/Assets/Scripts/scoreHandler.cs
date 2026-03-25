1d1cf3efea9eb810ccd09c24f79d6178fdaa1c5b
Kyra McCandless <mail@kyramccandless.com>
2026-03-24 21:45:25 -0400

    Adding basic scoring scene
    Since we haven't implemented item tracking yet, I just set the correct and incorrect item counts to arbitrary numbers. I also set the points awarded and penalized to random numbers as well.

---
+++ Shop For Mom/Assets/Scripts/scoreHandler.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreHandler : MonoBehaviour
+{
+    // Score texts
+    public TMP_Text totalScoreText;
+    public TMP_Text correctItemsText;
+    public TMP_Text incorrectItemsText;
+
+    // Settings
+    public int correctPoints = 100;
+    public int incorrectPoints = 50;
+
+    // These are private so other scripts can't accidentally 
+    // mess them up without using our methods
+    private int correctCount = 10;
+    private int incorrectCount = 2;
+
+    void Start()
+    {
+        // Calculate the total score
+        int totalScore = correctCount*correctPoints - incorrectCount*incorrectPoints;
+        // Update the text displays
+        UpdateText(totalScoreText, $"Total Score: {totalScore}");
+        UpdateText(correctItemsText, $"Correct Items: {correctCount}");
+        UpdateText(incorrectItemsText, $"Incorrect Items: {incorrectCount}");
+    }
+
+    private void UpdateText(TMP_Text textObject, string message)
+    {
+        if (textObject != null) {
+            textObject.text = message;
+        }
+        else {
+            Debug.Log("Tried to update null text");
+        }
+    }
+}
