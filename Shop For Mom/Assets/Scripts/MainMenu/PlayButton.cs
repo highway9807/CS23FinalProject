2c48916b8d906a0da5d4c3938fef7c3d06ee0ed9
StanPanH <164770378+StanPanH@users.noreply.github.com>
2026-04-01 04:44:22 +0800

    buttons

---
+++ Shop For Mom/Assets/Scripts/MainMenu/PlayButton.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+	public float speed = 5f;
+	public float targetY = 12f;
+	public float moveDuration = 2f;
+
+	public float rotateSpeed = 100f;
+	public float targetAngle = 90f;
+
+	private float remainingAngle = 0f;
+
+	//public AudioSource moveAudio;
+
+	private bool isRunning = false;
+	private float timer = 0f;
+	private RectTransform rect;
+
+
+	public void StartRotating()
+	{
+		if(remainingAngle <= 0)
+		{
+			remainingAngle = targetAngle;
+		}
+	}
+
+	void Start()
+	{
+		rect = GetComponent<RectTransform>();
+	}
+
+	void Update()
+	{
+		if (isRunning)
+		{
+			if(remainingAngle > 0)
+			{
+				float rotationThisFrame = rotateSpeed * Time.deltaTime;
+				if(rotationThisFrame > remainingAngle)
+				{
+					rotationThisFrame = remainingAngle;
+				}
+				transform.Rotate(0,0,-rotationThisFrame);
+				remainingAngle -= rotationThisFrame;
+			}
+			
+			rect.anchoredPosition += Vector2.down*speed*Time.deltaTime;
+			
+			//transform.Translate(Vector3.up * speed * Time.deltaTime);
+			timer += Time.deltaTime;
+			
+			//SceneManager.LoadScene(1);
+			if (timer >= moveDuration)
+			{
+				isRunning = false;
+				//moveAudio.Stop();
+				SceneManager.LoadScene(4);
+			}
+		}
+	}
+
+
+	public void GameStart()
+	{
+		isRunning = true;
+		timer = 0f;
+		//moveAudio.Play();
+		
+	}
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
