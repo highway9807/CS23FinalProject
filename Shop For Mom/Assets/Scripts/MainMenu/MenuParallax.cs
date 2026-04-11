eb928c7cc442ca7bd8aafe8bbb58233d7ab3ab93
StanPanH <164770378+StanPanH@users.noreply.github.com>
2026-04-12 03:25:43 +0800

    Adding parallax fixer

--- Shop For Mom/Assets/Scripts/MainMenu/MenuParallax.cs
+++ Shop For Mom/Assets/Scripts/MainMenu/MenuParallax.cs
@@ -8,18 +8,34 @@ public class MenuParallax : MonoBehaviour
     private Vector2 startPosition;
     private Vector3 velocity;
 
-
+    private Vector3 Position;
+    private bool prevScreen;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPosition = transform.position;
+        prevScreen = Screen.fullScreen;
+        Reposition();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Screen.fullScreen != prevScreen)
+        {
+            prevScreen = Screen.fullScreen;
+            Reposition();
+        }
+
         Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+
+    }
+
+    void Reposition()
+    {
+        Position = transform.position;
     }
 }
