d2f977453555802530a1152850b6a24a6e877160
StanPanH <164770378+StanPanH@users.noreply.github.com>
2026-04-21 16:24:11 +0800

    Level Select Scene basic functions

---
+++ Shop For Mom/Assets/Scripts/LevelSelect/SelectFunction.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectFunction : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void SLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void SLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void SLevel4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void SLevel5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+
+    public void SLevel6()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+
+}
