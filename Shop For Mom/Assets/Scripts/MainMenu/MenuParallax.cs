c1d9cda6cc46bbb561b165c831b7706f3959994a
StanPanH <164770378+StanPanH@users.noreply.github.com>
2026-04-27 07:04:44 +0800

    small renames

--- Shop For Mom/Assets/Scripts/MainMenu/MenuParallax.cs
+++ Shop For Mom/Assets/Scripts/MainMenu/MenuParallax.cs
@@ -2,40 +2,38 @@ using UnityEngine;
 
 public class MenuParallax : MonoBehaviour
 {
+    //varaibles for parallax effect
     public float offsetMultiplier = 1f;
     public float smoothTime = .3f;
-
     private Vector2 startPosition;
     private Vector3 velocity;
 
-    private Vector3 Position;
-    private bool prevScreen;
+    RectTransform rect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPosition = transform.position;
-        prevScreen = Screen.fullScreen;
-        Reposition();
+        rect = GetComponent<RectTransform>();
+        //startPosition = rect.localPosition;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Screen.fullScreen != prevScreen)
-        {
-            prevScreen = Screen.fullScreen;
-            Reposition();
-        }
-
+        
+        //Parallax function
         Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
 
     }
 
-    void Reposition()
+    void OnRectTransformDimensionChange()
     {
-        Position = transform.position;
+        if (rect != null)
+        {
+            rect.localPosition = startPosition;
+        }
     }
 }
