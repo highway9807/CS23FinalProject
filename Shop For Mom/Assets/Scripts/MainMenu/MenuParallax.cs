1a4a9445f379987577d37f07b65c1846cad6492c
Darmatage <JasonWiserArt@gmail.com>
2026-04-27 00:23:45 -0400

    Fixed main menu parallax reset issue!

--- Shop For Mom/Assets/Scripts/MainMenu/MenuParallax.cs
+++ Shop For Mom/Assets/Scripts/MainMenu/MenuParallax.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MenuParallax : MonoBehaviour
 {
     //varaibles for parallax effect
     public float offsetMultiplier = 1f;
+	float offsetStartMultiplyer;
     public float smoothTime = .3f;
     private Vector2 startPosition;
     private Vector3 velocity;
 
+	//screen resize reset:
+	private int lastWidth;
+    private int lastHeight;
 
-	Vector3 originalPos;
-    RectTransform rect;
-	Vector2 lastSize;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPosition = transform.position;
-        rect = GetComponent<RectTransform>();
-		originalPos = rect.localPosition;
-		lastSize = rect.rect.size;
-        //startPosition = rect.localPosition;
+		offsetStartMultiplyer = offsetMultiplier;
         
+		//screen resize reset:
+		lastWidth = Screen.width;
+        lastHeight = Screen.height;
     }
 
     // Update is called once per frame
@@ -31,18 +34,24 @@ public class MenuParallax : MonoBehaviour
         Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
 
+		//screen resize reset:
+		if (Screen.width != lastWidth || Screen.height != lastHeight) {
+            lastWidth = Screen.width;
+            lastHeight = Screen.height;
+            OnScreenResized();
+        }		
     }
 
-	//Called automatically on screen resize
-    void OnRectTransformDimensionsChange()
-    {
-		Debug.Log("current rect size: " + rect.rect.size);
-        if ((rect != null) && (rect.rect.size != lastSize))
-        {
-			Debug.Log("current rect size: " + rect.rect.size);
-            rect.localPosition = originalPos;
-			lastSize = rect.rect.size;
-			//rectTransform.rect.size;
-        }
+//screen resize reset:
+	void OnScreenResized() {
+		startPosition = new Vector2 (Screen.width / 2,  Screen.height / 2);
+		float resizeMuliplyer = (float)Screen.width / 1280f;
+		offsetMultiplier = offsetStartMultiplyer * resizeMuliplyer;
+		if (offsetMultiplier < offsetStartMultiplyer)
+		{
+			offsetMultiplier = offsetStartMultiplyer;
+		}
+		Debug.Log(" offsetMultiplyer = " + offsetMultiplier + ", " + offsetStartMultiplyer + ", " + Screen.width + " " + resizeMuliplyer);
     }
+
 }
